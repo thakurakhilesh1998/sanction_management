478e8bf5788e02e98ca2f690fe74abf528a75e13
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2024-01-02 21:26:29 +0530

    footer changes

--- resources/views/layouts/inc/frontend-footer.blade.php
+++ resources/views/layouts/inc/frontend-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="py-2 bg-gray">
     <div class="container text-center">
-        &copy; Copyright at <a href="www.techbythakur.com">Department of Panchayait Raj</a>
+        &copy; Copyright at <a href="https:://www.hppanchayat.nic.in">Department of Panchayait Raj</a>
         All right reserved.
         Designed and Developed by Department of Panchayati Raj.
     </div>
