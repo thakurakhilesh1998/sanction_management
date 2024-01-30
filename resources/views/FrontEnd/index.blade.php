d8c373cdfb92d4a853f13c3d4ce690399e9828bb
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2024-01-30 13:19:52 +0530

    Minor changes

--- resources/views/FrontEnd/index.blade.php
+++ resources/views/FrontEnd/index.blade.php
@@ -82,18 +82,18 @@
             <h3>Sanction Management System</h3>
         </div>  
         <div class="row card-body">
-            <div class="col md-4 card m-1 p-1">
-                <p>
+            <div class="col md-4 m-1 p-1">
+                <p class="text-center">
                     Introducing the Sanction Management System, a cutting-edge initiative by the Department of Panchayati Raj, Government of Himachal Pradesh. Crafted with precision, this software is designed to streamline the allocation of funds sanctioned by the department to districts under the State Finance Commission.
                 </p>
             </div>
-            <div class="col-md-4 card m-1 p-1">
-                <p>
+            <div class="col-md-4 m-1 p-1">
+                <p class="text-center">
                     Our sophisticated system not only simplifies fund management for both the Directorate office and District offices but also introduces a new level of transparency and efficiency to governance. Districts now have the capability to seamlessly upload Utilization Certificates (UC) and images associated with the sanctioned funds, offering a comprehensive view of financial utilization.
                 </p>
             </div>
-            <div class="col-md-4 card m-1 p-1">
-                <p>
+            <div class="col-md-4 m-1 p-1">
+                <p class="text-center">
                     What sets us apart is our commitment to public accessibility. The software opens up a wealth of information, making crucial parameters available to the public domain. Citizens can effortlessly access details about their District, Block, and Gram Panchayat, fostering a governance model that is transparent, efficient, and citizen-friendly. Empowering communities with information, we believe in transforming governance for the betterment of all."
                 </p>
             </div>
